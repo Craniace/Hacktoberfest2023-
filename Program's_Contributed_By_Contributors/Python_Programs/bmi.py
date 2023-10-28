4326391474830460704000ef87c33abef10c1e3c
Sarthak Joshi <100042684+Craniace@users.noreply.github.com>
2023-10-28 14:31:54 +0530

    bmi.py in Python_Programs

---
+++ Program's_Contributed_By_Contributors/Python_Programs/bmi.py
@@ -0,0 +1,9 @@
+#To calculate BMI
+weight_in_kg=float(input("Enter the weight :"))
+height_in_m=float(input("Enter the height :"))
+bmi=weight_in_kg/(height_in_m*height_in_m)
+print("BMI is :",bmi)
+
+
+
+
